3de8218ffb0a6279ba8198616c0a7c81c6a8694c
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2024-06-21 20:54:47 -0600

    adding association N to N between rewards and tasks through: RewardProduct

--- Models/associations.js
+++ Models/associations.js
@@ -3,6 +3,7 @@ import { Product } from "./Products.Model.js";
 import { Providers } from "./Providers.Model.js";
 import { Rewards } from "./Reward.Model.js";
 import { Task } from "./Task.Models.js";
+import { RewardProduct } from "./Reward-Task.model.js";
 
 const initializeAssociations = () => {
 
@@ -13,7 +14,9 @@ const initializeAssociations = () => {
     Providers.hasMany(Store, { as: "store", foreignKey: "providerId" })
     Store.belongsTo(Providers, { as: "provider" })
 
-    
+    //A Reward can have many tasks and a task can have many rewards
+    Rewards.belongsToMany(Task, {as: "task", through: RewardProduct, foreignKey: "rewardId"});
+    Task.belongsToMany(Rewards, {as: "reward", through: RewardProduct, foreignKey: "taskId"});
 
 }
 
