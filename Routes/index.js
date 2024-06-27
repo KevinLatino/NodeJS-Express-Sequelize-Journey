60bd30358ed59f014e1f02123ad678856bae44f9
KevinLatino <latinokevin9@gmail.com>
2024-06-27 10:52:52 -0600

    Auth added

--- Routes/index.js
+++ Routes/index.js
@@ -3,6 +3,7 @@ import StoreRoutes from './Store.Routes.js'
 import ProviderRoutes from './Provider.Routes.js'
 import RewardRoutes from './Reward.Routes.js'
 import UserRoutes from './User.Routes.js'
+import AuthRoutes from './Auth.Routes.js'
 
 const routerApi = (app) => {
     app.use('/products', ProductRoutes);
