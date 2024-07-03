5c0471ebec0661305235c08064addaefd710a669
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2024-07-03 16:35:24 -0600

    Creating a JWT strategy and including a middleware on route providers/list as an example

--- Auth/initStrategies.js
+++ Auth/initStrategies.js
@@ -1,8 +1,10 @@
 import passport from "passport";
 import localStrategy from "./strategies/local.strategy.js";
+import jwtStrategy from "./strategies/jwt.strategy.js";
 
 const initStrategies = () => {
     passport.use(localStrategy);
+    passport.use(jwtStrategy);
 }
 
 export default initStrategies;
--- Routes/Provider.Routes.js
+++ Routes/Provider.Routes.js
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import ProviderServices from "../Services/Provider.Services.js";
+import passport from "passport";
 
 const Services = new ProviderServices();
 const router = Router();
 
-router.get('/list', async (req, res) => {
-    const getProviders = await Services.getAllProviders();
-    res.json(getProviders);
-})
+router.get('/list',
+    passport.authenticate('jwt', {session: false}),
+    async (req, res) => {
+        const getProviders = await Services.getAllProviders();
+        res.json(getProviders);
+    })
 
 router.post('/create', async (req, res) => {
     const body = req.body
