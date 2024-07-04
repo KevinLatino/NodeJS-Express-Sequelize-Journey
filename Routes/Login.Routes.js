abdc3a069dcf4d03ed311f0d40d206d7d8605c8b
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2024-07-04 17:18:12 -0600

    adding a node mailer for sending emails

--- Routes/Login.Routes.js
+++ Routes/Login.Routes.js
@@ -5,11 +5,21 @@ import AuthServices from "../Services/Auth.Services.js";
 const authService = new AuthServices();
 const router = Router();
 
-router.post('/login', passport.authenticate("local", {session: false}), async (req, res, next) => {
+router.post('/login', passport.authenticate("local", { session: false }), async (req, res, next) => {
     try {
         const user = req.user
         const token = await authService.signJwt(user)
         res.json({ user, token })
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.post('/recovery', async (req, res, next) => {
+    try {
+        const { email } = req.body
+        const recoverEmail = await authService.sendEmail(email)
+        res.json(recoverEmail)
     } catch (error) {
         next(error)
     }
--- Services/Auth.Services.js
+++ Services/Auth.Services.js
@@ -1,4 +1,5 @@
 import UserServices from "../Services/User.Model.js";
+import nodemailer from "nodemailer"
 import bcrypt from 'bcrypt'
 import boom from "@hapi/boom";
 import jwt from 'jsonwebtoken'
@@ -31,6 +32,34 @@ class AuthServices {
 
         const token = jwt.sign(payload, secret)
         return token
+    }
+
+    async sendEmail(email) {
+        try {
+            const user = await userServices.findByEmail(email);
+            if (!user) {
+                throw boom.unauthorized();
+            }
+    
+            const transporter = nodemailer.createTransport({
+                host: "smtp.gmail.com",
+                port: 587,
+                auth: {
+                    user: process.env.USER,
+                    pass: process.env.PASS,
+                },
+            })
+            await transporter.sendMail({
+                from: process.env.USER,
+                to: `${user.email}`,
+                subject: "Probando correo",
+                text: `Hola ${user.name}`,
+                html: "<b>Arley Manco</b>"
+            })
+            return {message: "Mail sent"}   
+        } catch (error) {
+            return {message: "couldnt sent"}
+        }
     }
 }
 
