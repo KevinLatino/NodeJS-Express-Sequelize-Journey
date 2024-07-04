3ae16fc20e239de4ae5cc930adbf0102998e6bc3
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2024-07-04 13:48:11 -0600

    Changes on Auth.Services.js

--- Routes/Login.Routes.js
+++ Routes/Login.Routes.js
@@ -5,14 +5,6 @@ import AuthServices from "../Services/Auth.Services.js";
 const authService = new AuthServices();
 const router = Router();
 
-router.post('/login', passport.authenticate("local", { session: false }), async (req, res, next) => {
-    try {
-        const user = req.user
-        const token = await authService.signJWT(user);
-        res.json({user, token})
-    } catch (error) {
-        next(error)
-    }
-})
+
 
 export default router;
--- Services/Auth.Services.js
+++ Services/Auth.Services.js
@@ -10,27 +10,27 @@ const userServices = new UserServices()
 
 class AuthServices {
 
-    async findUser (email, password) {
-        //
+    async findUser(email, password) {
+        //here we can find the email of a user
         const user = await userServices.findByEmail(email);
-        if(!user){
+        if (!user) {
             throw boom.unauthorized();
         }
+        //here we can find the password of a user
         const passwordCompare = await bcrypt.compare(password, user.password);
-        if(!passwordCompare){
+        if (!passwordCompare) {
             throw boom.unauthorized();
         }
         delete user.dataValues.password
         return user
     }
 
-    async signJWT(user) {
-        const payload = {
-            sub: user.id
-        }
+    async signJwt() {
+        const payload = { sub: user.id }
         const secret = process.env.JWT_SECRET
+
         const token = jwt.sign(payload, secret)
-        return token;
+        return token
     }
 }
 
